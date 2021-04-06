c7401ad2b1648e9996abdcac49a7c83f4afea5bf
Aaron Teo <aaron.teo@riv-alumni.com>
2021-04-06 15:25:49 +0800

    Added comments to index file

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,15 @@ import { initializeApp } from 'firebase-admin';
 
 initializeApp();
 
+/**
+ * Firebase Authentication Listeners
+ */
 export { authUsersOnCreate } from './auth/users/onCreate';
 export { authUsersOnDelete } from './auth/users/onDelete';
+
+/**
+ * Firestore Document Listeners
+ */
 export { firestoreUsersOnCreate } from './firestore/users/onCreate';
+export { firestoreMembersOnCreate } from './firestore/members/onCreate';
+export { firestoreMembersOnDelete } from './firestore/members/onDelete';
